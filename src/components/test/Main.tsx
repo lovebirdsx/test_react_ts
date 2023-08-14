4afe24987debe11e99a7fb30a70307a9cb304048
lovebird <lovebirdsx@gmail.com>
2023-08-15 00:30:48 +0800

    优化Grid表现

--- src/components/test/Main.tsx
+++ src/components/test/Main.tsx
@@ -1,5 +1,5 @@
 import container from '../../inversify.config'
-import { Box, FormControlLabel, Radio, Typography } from '@mui/material'
+import { Box, FormControlLabel, Grid, Radio, Typography } from '@mui/material'
 import { ReactTestManager } from '../../common/test/ReactTestManager';
 import { GraphEditor } from './GraphEditor';
 import { TestContext } from './TestContext';
@@ -11,7 +11,7 @@ import { Counter } from '../../features/counter/Counter';
 
 function registerAllTests() {
   const manager = container.resolve(ReactTestManager);
-  manager.registerTest('TestHello', () => <Hello/>);
+  manager.registerTest('TestHello', () => <Hello />);
   manager.registerTest('TestContext', () => <TestContext />);
   manager.registerTest('GraphEditor', () => <GraphEditor />);
   manager.registerTest('TestImmer', () => <TestImmer />);
@@ -21,9 +21,11 @@ function registerAllTests() {
 
 function renderTestTitle(props: { name: string, select: boolean, onClick: () => void }) {
   return (
-    <FormControlLabel value={props.name} label={props.name} control={
-      <Radio name='test' checked={props.select} defaultChecked={props.select} onChange={props.onClick} />
-    } />
+    <Grid xs>
+      <FormControlLabel value={props.name} label={props.name} control={
+        <Radio name='test' checked={props.select} defaultChecked={props.select} onChange={props.onClick} />
+      } />
+    </Grid>
   );
 }
 
@@ -35,9 +37,9 @@ export function TestMain() {
     <Box p={2}>
       <Typography variant='h2'>React Test</Typography>
       <Box py={2}>
-        <Box p={2} bgcolor={'lightgray'}>
+        <Grid container p={2} bgcolor={'lightgray'}>
           {manager.tests.map((e, idx) => renderTestTitle({ name: e.name, select: idx === select, onClick: () => setSelect(idx) }))}
-        </Box>
+        </Grid>
         <Box p={2}>
           {manager.tests[select].render()}
         </Box>
