b961bff9b28d61e873bdc3e9a027b8fa86cd3d6c
lovebird <lovebirdsx@gmail.com>
2023-11-19 20:43:04 +0800

    测试滚动条

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,16 @@ import { SinglePostPage } from './features/posts/SinglePostPage';
 import { PostsList } from './features/posts/PostList';
 import { NavBar } from './app/Navbar';
 import { EditPostForm } from './features/posts/EditPostForm';
+import { Box } from '@mui/material';
 
-class App extends React.Component {
+export class App extends React.Component {
   render(): React.ReactNode {
     return (
       <Provider store={store}>
         <BrowserRouter>
-        <NavBar />
+          <NavBar />
           <Routes>
-            <Route path='/'  Component={PostsList} />
+            <Route path='/' Component={PostsList} />
             <Route path='/tests' Component={TestMain} />
             <Route path='/posts' Component={PostsList} />
             <Route path='/posts/:postId' Component={SinglePostPage} />
@@ -25,6 +26,45 @@ class App extends React.Component {
       </Provider>
     );
   }
+}
+
+// 测试滚动条
+export function TestScroll() {
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+      <Box sx={{ backgroundColor: 'primary.main', p: 2 }}>
+        Banner 内容
+      </Box>
+      <Box sx={{ flex: 1, display: 'flex', height: 0 }}>
+
+        <Box sx={{ flex: 1, overflowY: 'auto', height: '100%', p: 2 }}>
+          <Box height={400} bgcolor={'gray'} p={2} />
+        </Box>
+
+        <Box sx={{ flex: 1, overflowY: 'auto', height: '100%', p: 2 }}>
+          <Box height={800} bgcolor={'gray'} p={2} />
+        </Box>
+
+        <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column', p: 2 }}>
+          <Box height={200} bgcolor={'gray'} p={2} />
+          <Box sx={{ flex: 1, overflowY: 'auto', height: '100%', p: 2 }}>
+            <Box height={800} bgcolor={'gray'} p={2} />
+          </Box>
+        </Box>
+
+        <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column', p: 2 }}>
+          <Box height={200} bgcolor={'gray'} p={2} />
+          <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column', overflowY: 'hidden', p: 2 }}>
+            <Box height={200} bgcolor={'gray'} p={2} />
+            <Box sx={{ flex: 1, overflowY: 'auto', height: '100%', p: 2 }}>
+              <Box height={400} bgcolor={'gray'} p={2} />
+            </Box>
+          </Box>
+        </Box>
+
+      </Box>
+    </Box>
+  );
 }
 
 export default App;
