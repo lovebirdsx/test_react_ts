2845ffaeeced8ebec2980a3a8754ec1464b7bc75
lovebird <lovebirdsx@gmail.com>
2024-10-21 22:15:34 +0800

    up

--- src/components/test/TestReactWindow.tsx
+++ src/components/test/TestReactWindow.tsx
@@ -7,16 +7,30 @@ const Row: React.FC<{ index: number; style: CSSProperties }> = ({ index, style }
   </div>
 );
 
-const Example = () => (
-  <List className="List" height={150} itemCount={100000} itemSize={20} width={300}>
+const Example: React.FC<{ itemCount: number }> = ({ itemCount }) => (
+  <List className="List" height={150} itemCount={itemCount} itemSize={20} width={300}>
     {Row}
   </List>
 );
 
+const VariableSizeList: React.FC = () => {
+  const [itemCount, setItemCount] = React.useState(10000);
+  return (
+    <div>
+      <Example itemCount={itemCount} />
+      {itemCount} items
+      <div>
+        <button onClick={() => setItemCount(itemCount + 100)}>Add 100 rows</button>
+        <button onClick={() => setItemCount(Math.max(itemCount - 100, 0))}>Remove 100 rows</button>
+      </div>
+    </div>
+  );
+};
+
 export const TestReactWindow: React.FC = () => {
   return (
     <div>
-      <Example />
+      <VariableSizeList />
     </div>
   );
 };
