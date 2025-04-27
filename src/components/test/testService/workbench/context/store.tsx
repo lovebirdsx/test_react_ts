b5a2945f55667c0c3df58b7adf19829439a3bd17
lovebird <lovebirdsx@gmail.com>
2025-04-27 16:34:39 +0800

    解决在strict模式下，store的资源管理问题

--- src/components/test/testService/workbench/context/store.tsx
+++ src/components/test/testService/workbench/context/store.tsx
@@ -1,7 +1,7 @@
 import { createStore } from 'zustand';
 import { useStoreWithEqualityFn } from 'zustand/traditional';
 import { shallow } from 'zustand/shallow';
-import { createContext, useContext, useEffect, useState } from 'react'; // Import useState
+import { createContext, useContext, useEffect, useRef, useState } from 'react'; // Import useState
 import { WorkbenchStore, workbenchStoreCreator } from '../store/store';
 import { IWorkbenchStoreOptions } from '../store/common';
 import { DisposableStore } from '../../base/event';
@@ -13,35 +13,31 @@ export const createWorkbenchStore = (options: IWorkbenchStoreOptions) => {
 
 export type WorkbenchStoreApi = ReturnType<typeof createWorkbenchStore>;
 
-let workbenchStore: WorkbenchStoreApi | undefined;
-function getWorkbenchStore(options: IWorkbenchStoreOptions) {
-  if (!workbenchStore) {
-    workbenchStore = createWorkbenchStore(options);
-  }
-  return workbenchStore;
-}
-
 const WorkbenchStoreContext = createContext<WorkbenchStoreApi>(undefined!);
 
 export function WorkbenchStoreProvider({ children }: { children: React.ReactNode }) {
   const accessor = useService();
+  const storeRef = useRef<WorkbenchStoreApi | null>(null);
+  const [store, setStore] = useState<WorkbenchStoreApi>();
 
-  // 只在初次渲染时执行一次
-  const [{ store, disposable }] = useState(() => {
+  // 在strict模式下，useEffect会多执行一次，所以要确保资源创建和销毁的一致性
+  // 具体参考：https://legacy.reactjs.org/docs/strict-mode.html
+  useEffect(() => {
     const disposable = new DisposableStore();
     const store = createWorkbenchStore({ accessor, disposableStore: disposable });
-    console.log('create store', store.getState().id);
-    return { store, disposable };
-  });
+    storeRef.current = store;
+    setStore(store);
 
-  // 只在卸载时清理一次
-  useEffect(() => {
-    console.log('use effect', store.getState().id);
     return () => {
       disposable.dispose();
-      console.log('dispose store', store.getState().id);
+      storeRef.current = null;
     };
-  }, [disposable, store]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!store) {
+    return <div>Loading...</div>;
+  }
 
   return <WorkbenchStoreContext.Provider value={store}>{children}</WorkbenchStoreContext.Provider>;
 }
